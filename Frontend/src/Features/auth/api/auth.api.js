c0131f8a7367b7b7b1f457152cca2b0588a67191
codedinpeace <vea5a.pranam05@gmail.com>
2026-04-15 18:56:53 +0530

    added api functions

---
+++ Frontend/src/Features/auth/api/auth.api.js
@@ -0,0 +1,36 @@
+import { api } from "../../../utils/api";
+
+export const register = async (username,email,password) =>{
+    try {
+        const response = await api.post("/auth/register", {username,email,password})
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const login = async (identifier, password) => {
+    try {
+        const response = await api.post("/auth/login", {identifier, password})
+        return response.data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const logout = async () => {
+    try {
+        await api.post("/auth/logout")  
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const check = async () => {
+    try {
+        const response = await api.get("/auth/check")
+        return response.data
+    } catch (error) {
+        console.log(error)        
+    }
+}
